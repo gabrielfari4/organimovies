e0790b372226919fb45b958e1d7038df8c2f4181
Gabriel Faria <gabrielfaria88@gmail.com>
2024-07-25 17:32:52 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+const Header = () => {
+    return (
+        <section className='header'>
+            <div>
+                
+            </div>
+        </section>
+    )
+}
+
+export default Header;
