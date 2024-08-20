8b0ee385e13850bd207004bb752900cdc62fa1e4
Gabriel Faria <gabrielfaria88@gmail.com>
2024-08-20 15:13:24 -0300

    refactor: refatoring movie list logic

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,115 +1,129 @@
-import { useEffect, useState } from 'react';
-import Input from '../Input';
-import './styles.css'
-import Button from '../Button';
-import Rate from '../Rate';
-import APIKey from '../../config/key';
+import { useEffect, useState } from "react";
+import Input from "../Input";
+import "./styles.css";
+import Button from "../Button";
+import Rate from "../Rate";
+import APIKey from "../../config/key";
 
 const Form = (props) => {
+  const [name, setName] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [rating, setRating] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [genreList, setGenreList] = useState([]);
+  const [genreArray, setGenreArray] = useState([]);
+  const [movieId, setMovieId] = useState("");
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
 
-    const [name, setName] = useState('');
-    const [genres, setGenres] = useState([]);
-    const [rating, setRating] = useState('');
-    const [movies, setMovies] = useState([])
-    const [genreList, setGenreList] = useState([]);
-    const [genreArray, setGenreArray] = useState([]);
-    const [movieId, setMovieId] = useState('');
-
-    const fetchMovie = async () => {
-        try {
-            const data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${APIKey}`)
-            const response = await data.json()
-            console.log(response.results)
-            console.log(name)
-            setMovies(response.results)
-        } catch (error) {
-            console.error("Erro ao buscar o filme:", error);
-        }
+  const fetchMovie = async () => {
+    try {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${APIKey}`
+      );
+      const response = await data.json();
+      console.log(response.results);
+      console.log(name);
+      setMovies(response.results);
+    } catch (error) {
+      console.error("Erro ao buscar o filme:", error);
     }
+  };
 
-    const fetchGenre = async () => {
-        try {
-            const data = await fetch(`https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${APIKey}`)
-            const response = await data.json()
-            setGenreList(response.genres)
-        } catch (e) {
-            console.error("Erro ao buscar o filme:", e);
-        }
+  const fetchGenre = async () => {
+    try {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${APIKey}`
+      );
+      const response = await data.json();
+      setGenreList(response.genres);
+    } catch (e) {
+      console.error("Erro ao buscar o filme:", e);
     }
-    
-    const onSave = (e) => {
-        e.preventDefault()
-        props.onSubmittingMovie({
-            name,
-            genres,
-            rating,
-            genreArray,
-            movieId
-        })
-        setName('');
-        setGenres([]);
-        setRating('');
-        setGenreArray([]);
-        setMovieId('')
-    }
-    
-    const Info = () => {
-        console.log(name, genres, rating, genreArray, movieId)
-    }
+  };
 
-    useEffect(() => {
-        const updatedGenreArray = genreList.filter((genre) => genres.includes(genre.id));
-        setGenreArray(updatedGenreArray);
-    }, [genres, genreList]);
-    
-    
-    return (
-        <section className="form">
-            <form onSubmit={onSave}>
-                <h2>Preencha os dados para classificar os filmes</h2>
-               
-                    <Input label='Nome' placeholder='Busque o nome do filme' onChanged={value => setName(value)} value={name} onClicked={() => {
-                        fetchMovie() 
-                        fetchGenre()
-                    }
-                    }/>
+  const onSave = (e) => {
+    e.preventDefault();
+    props.onSubmittingMovie({
+      name,
+      genres,
+      rating,
+      genreArray,
+      movieId,
+    });
+    setName("");
+    setGenres([]);
+    setGenreArray([]);
+    setMovieId("");
+    setSelectedMovieId("");
+  };
 
-                <div className='moviePosters'>
-                    {movies.map((movie) => {
-                        return (
-                            <> 
-                            {/* TODO: incluir estilo (borda?) quando selecionado */}
-                                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Poster do filme ${movie.title}`} title={movie.title} key={movie.id} onClick={() => {
-                                    setName(movie.title)
-                                    setGenres(movie.genre_ids)
-                                    setMovieId(movie.id)
-                                }
-                                }/>
-                            </>
-                        )
-                    })}
-                </div>
+  const Info = () => {
+    console.log(name, genres, rating, genreArray, movieId);
+  };
 
-                {/* <div className='genre'>
-                    <h2>Gênero</h2>
-                    <ul>
-                        {genreArray.map((gnr) => {
-                            return (
-                                <li>{gnr.name}</li>
-                            )
-                        })}
-                    </ul>
-                </div> */}
-                
-                <Rate
-                    value={rating}
-                    onClicked={value => {
-                    setRating(value)
-                } }/>
-                <Button onClicked={Info}>Classificar</Button>
-            </form>
-        </section>
-    )
-}
+  useEffect(() => {
+    const updatedGenreArray = genreList.filter((genre) =>
+      genres.includes(genre.id)
+    );
+    setGenreArray(updatedGenreArray);
+  }, [genres, genreList]);
+
+  return (
+    <section className="form">
+      <form onSubmit={onSave}>
+        <h2>Preencha os dados para classificar os filmes</h2>
+
+        <Input
+          label="Nome"
+          placeholder="Busque o nome do filme"
+          onChanged={(value) => setName(value)}
+          value={name}
+          onClicked={(e) => {
+            e.preventDefault()
+            fetchMovie();
+            fetchGenre();
+            setName("");
+            setGenres([]);
+            setGenreArray([]);
+            setMovieId("");
+            setSelectedMovieId("");
+          }}
+        />
+
+        <div className="moviePosters">
+          {movies.map((movie) => {
+            const imgClass = movie.id === selectedMovieId ? "selected" : "";
+            return (
+              <>
+                <img
+                  className={imgClass}
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={`Poster do filme ${movie.title}`}
+                  title={movie.title}
+                  key={movie.id}
+                  onClick={() => {
+                    setName(movie.title);
+                    setGenres(movie.genre_ids);
+                    setMovieId(movie.id);
+                    setSelectedMovieId(movie.id);
+                  }}
+                />
+              </>
+            );
+          })}
+        </div>
+
+        <Rate
+          value={rating}
+          onClicked={(value) => {
+            setRating(value);
+          }}
+        />
+        
+        <Button onClicked={Info}>Classificar</Button>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
